00d06d534df9b20bc3b6ac75cd08a8cde45d4981
Joseph880929 <gentlemanjos1@gmail.com>
2024-01-09 17:39:44 -0500

    PEC6 - Archivo README.md diligenciado y refinamiento de clase modelo
    Se diligencia el archivo README.md y se refina la clase modelo Article.

--- PEC6_Ej_Prac/ecommerce/src/app/model/article.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/model/article.ts
@@ -1,5 +1,5 @@
 // Clase modelo para componente article-item
-export class Article {   
+export class Article {
     // Constructor de la clase modelo
     constructor(
         public id: number,              // Id del artículo
@@ -7,17 +7,6 @@ export class Article {
         public imageUrl: string,        // URL de la ubicación de la imagen del artículo (en la carpeta assets)
         public price: number,           // Costo unitario del artículo.
         public isOnSale: boolean,       // Indica si el artículo se encuentra disponible para la venta.        
-        public quantityInCart: number   // Cantidad de existencias del artículo en inventario.
-        ) { }
-
-        // Codigo correspondiente a la solución del ejercicio 3, para incrementar y decrementar stock del artículo.
-        // Método modificado para aumentar stock del artículo.
-        /* aumentarStock(): void {
-            this.quantityInCart++;                               
-        }
-        
-        // Método para decrementar stock del artículo.
-        disminuirStock(): void {           
-            this.quantityInCart--;              
-        } */      
+        public quantityInCart: number   // Cantidad de existencias del artículo en inventario.        
+    ) { }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PEC6-Servicios en Angular
- 
+• Login UOC: jparejasal
+• Estudiante: Jose Luis Pareja Salinas
+• URL Repositorio: https://github.com/jparejasal/PEC6-Servicios-en-Angular.git
+• La PEC6 enfatiza en los siguientes aspectos:
+* Definición y aplicación de conceptos teóricos fundamentales para el entendimiento de creación y uso de servicios en Angular CLI.
+* Implementación de servicios dinámicos que permitan la funcionalidad de aplicativos en Angular CLI.
+* Identificación y comprensión de como comunicar servicios con componentes en Angular CLI.
